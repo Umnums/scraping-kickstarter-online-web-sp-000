258b2be2f0ec02dcaf76c147bb9b7a9af23901eb
Joseph Kao <josephtkao@gmail.com>
2020-04-20 22:43:28 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,7 @@ def create_project_hash
   #image = projects.css("img.projectphoto-little").attribute("src").text
   #title = project.css("h2.bbcard_name strong a").text
   #location = projects.css("span.location-name").text
+  percent = projects.css("li.first funded strong").text
 
 end
 
