82a662e83df768b6a4ea638eb48098876c73d0c6
Joseph Kao <josephtkao@gmail.com>
2020-04-20 22:22:51 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,5 +2,9 @@
 require 'Nokogiri'
 require 'pry'
 def create_project_hash
+  #this opens a file and reads it into a variable
+  html = File.read('fixtures/kickstarter.html')
+ 
+  kickstarter = Nokogiri::HTML(html)
 
 end
