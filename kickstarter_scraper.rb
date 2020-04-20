9b436bab923666e5e07854eae94e330e0694dea2
Joseph Kao <josephtkao@gmail.com>
2020-04-20 22:39:25 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,7 +4,10 @@ require "pry"
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  projects =  kickstarter.css("li.project.grid_4")
+  projects =  kickstarter.css("li.project.grid_4").first
+
+  #title = project.css("h2.bbcard_name strong a").text
+
 end
 
 create_project_hash
